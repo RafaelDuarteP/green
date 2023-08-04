f068beed052b7f8d264e744421586ed0e48839d0
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-08-04 14:34:09 -0300

    Organização das paginas

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once 'connections/Connection.php';
 require_once 'models/Cliente.php';
 require_once 'utils/converter.php';
+require_once 'utils/rotas.php';
 
 // Adiciona os assets
 include 'components/head.php';
@@ -12,30 +13,6 @@ Connection::getInstance();
 
 // Inicia a sessão
 session_start();
-
-// Define a lista de páginas disponíveis
-$pages = array(
-    'home' => 'pages/home.php',
-    'pedidos' => 'pages/pedidos.php',
-    'dados' => 'pages/dados.php',
-    'orcamentos' => 'pages/orcamentos.php',
-    'novo-orcamento' => 'pages/novo_orcamento.php',
-    'logout' => 'controllers/logoutController.php',
-    'auth/confirmacao' => 'controllers/confirmacaoController.php',
-    'confirmacao' => 'pages/confirmacao.php',
-    'controller/novo-orcamento' => 'controllers/novoOrcamentoController.php',
-);
-
-$access_pages = array(
-    'login' => 'pages/login.php',
-    'cadastro' => 'pages/cadastro.php',
-    'controller/login' => 'controllers/loginController.php',
-    'controller/cadastro' => 'controllers/cadastroController.php',
-    'auth/confirmacao' => 'controllers/confirmacaoController.php',
-    'confirmacao' => 'pages/confirmacao.php',
-);
-
-$control_pages = array();
 
 // Define a URL base do projeto
 define('BASE_URL', '/green/');
---
+++ utils/rotas.php
@@ -0,0 +1,24 @@
+<?php
+
+$pages = array(
+    'home' => 'pages/home.php',
+    'pedidos' => 'pages/pedidos.php',
+    'dados' => 'pages/dados.php',
+    'orcamentos' => 'pages/orcamentos.php',
+    'novo-orcamento' => 'pages/novo_orcamento.php',
+    'logout' => 'controllers/logoutController.php',
+    'auth/confirmacao' => 'controllers/confirmacaoController.php',
+    'confirmacao' => 'pages/confirmacao.php',
+    'controller/novo-orcamento' => 'controllers/novoOrcamentoController.php',
+);
+
+$access_pages = array(
+    'login' => 'pages/login.php',
+    'cadastro' => 'pages/cadastro.php',
+    'controller/login' => 'controllers/loginController.php',
+    'controller/cadastro' => 'controllers/cadastroController.php',
+    'auth/confirmacao' => 'controllers/confirmacaoController.php',
+    'confirmacao' => 'pages/confirmacao.php',
+);
+
+$control_pages = array();
