50c180bcbb52657f61b20da5d9a64874dcd93973
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-08-26 22:08:30 -0300

    tratamento de erros

---
+++ pages/500.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Oops Tivemos um erro</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #d9d9d9;
+        }
+
+        .container {
+            text-align: center;
+            padding: 30px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+
+        h1 {
+            color: #ffcc00;
+            font-size: 2em;
+            margin-bottom: 10px;
+        }
+
+        p {
+            color: #333;
+            font-size: 1.2em;
+        }
+
+        a {
+            color: #004226;
+            text-decoration: none;
+            font-weight: bold;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <h1 class="col-12 text-center">Erro interno</h1>
+            <div class="divisor-error"></div>
+            <p>Desculpe, ocorreu um erro no servidor. Por favor, tente novamente mais tarde ou entre em contato com o
+                suporte.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
